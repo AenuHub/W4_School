7cc160d223588c68ac7e3351645b846de6df5a04
AenuHub <jesuskane92@gmail.com>
2024-10-21 11:46:10 +0300

    update variables for clarity

--- W4_School/Program.cs
+++ W4_School/Program.cs
@@ -5,29 +5,29 @@ internal class Program
     private static void Main(string[] args)
     {
         // create different person objects and use them
-        Person john = new Person();
-        john.Name = "John";
-        john.Surname = "Doe";
-        john.BirthYear = 1964;
+        Person teacher1 = new Person();
+        teacher1.Name = "John";
+        teacher1.Surname = "Doe";
+        teacher1.BirthYear = 1964;
 
-        Person jane = new Person();
-        jane.Name = "Jane";
-        jane.Surname = "Dawson";
-        jane.BirthYear = 1968;
+        Person teacher2 = new Person();
+        teacher2.Name = "Jane";
+        teacher2.Surname = "Dawson";
+        teacher2.BirthYear = 1968;
 
-        Person peter = new Person();
-        peter.Name = "Peter";
-        peter.Surname = "Parker";
-        peter.BirthYear = 1990;
+        Person student1 = new Person();
+        student1.Name = "Peter";
+        student1.Surname = "Parker";
+        student1.BirthYear = 1990;
 
-        Person lucy = new Person();
-        lucy.Name = "Lucy";
-        lucy.Surname = "Newman";
-        lucy.BirthYear = 1992;
+        Person student2 = new Person();
+        student2.Name = "Lucy";
+        student2.Surname = "Newman";
+        student2.BirthYear = 1992;
 
-        Console.WriteLine($"{john.Name} {john.Surname} was born in {john.BirthYear}.");
-        Console.WriteLine($"{jane.Name} {jane.Surname} was born in {jane.BirthYear}.");
-        Console.WriteLine($"{peter.Name} {peter.Surname} was born in {peter.BirthYear}.");
-        Console.WriteLine($"{lucy.Name} {lucy.Surname} was born in {lucy.BirthYear}.");
+        Console.WriteLine($"{teacher1.Name} {teacher1.Surname} was born in {teacher1.BirthYear}.");
+        Console.WriteLine($"{teacher2.Name} {teacher2.Surname} was born in {teacher2.BirthYear}.");
+        Console.WriteLine($"{student1.Name} {student1.Surname} was born in {student1.BirthYear}.");
+        Console.WriteLine($"{student2.Name} {student2.Surname} was born in {student2.BirthYear}.");
     }
 }
